8f1a885caa81333d165da143d19d1e8d9f2ed5fd
Md. Mostak Shahid <mostak.shahid@gmail.com>
2026-06-02 01:48:13 +0600

    remove old files

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as LoginForm } from "./components/LoginForm";
-export { default as RegistrationForm } from "./components/RegistrationForm";
-export { default as NewsSideSheet } from "./components/NewsSideSheet";
-import "./data/menuItems";
